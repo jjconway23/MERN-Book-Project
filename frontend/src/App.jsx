936ba5af4abef47f6f0223ab8c5f07affeebb302
JJCONWAY23 <jacob.peat01@gmail.com>
2024-01-23 15:24:26 +0000

    create basic react app setup with no data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import ShowBook from './pages/ShowBook'
+import CreateBook from './pages/CreateBook'
+import DeleteBook from './pages/DeleteBook'
+import EditBook from './pages/EditBook'
+
 
 const App = () => {
   return (
-    <div className="bg-red-400 text-white">App</div>
+    
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/books/create' element={<CreateBook />}></Route>
+      <Route path='/books/details/:id' element={<ShowBook />}></Route>
+      <Route path='/books/edit/:id' element={<EditBook />}></Route>
+      <Route path='/books/delete/:id' element={<DeleteBook />}></Route>
+    </Routes>
   )
 }
 
