473ddfe89b23e16092d2c4dd5f8e28ccb5180e0f
JJCONWAY23 <jacob.peat01@gmail.com>
2024-01-23 18:21:46 +0000

    add functionality and view for editing book

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home'
 import ShowBook from './pages/ShowBook'
 import CreateBook from './pages/CreateBook'
 // import DeleteBook from './pages/DeleteBook'
-// import EditBook from './pages/EditBook'
+import EditBook from './pages/EditBook'
 
 
 const App = () => {
@@ -14,7 +14,7 @@ const App = () => {
       <Route path='/' element={<Home />}></Route>
       <Route path='/books/create' element={<CreateBook />}></Route>
       <Route path='/books/details/:id' element={<ShowBook />}></Route>
-      {/* <Route path='/books/edit/:id' element={<EditBook />}></Route> */}
+      <Route path='/books/edit/:id' element={<EditBook />}></Route>
       {/* <Route path='/books/delete/:id' element={<DeleteBook />}></Route> */}
     </Routes>
   )
