99f55d1ad1353ae552fc3a580ab0505fe3783fd7
JJCONWAY23 <jacob.peat01@gmail.com>
2024-01-23 13:43:56 +0000

    create route to update book

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,43 @@ app.get('/books', async (req, res)=> {
     }
 })
 
+//get a book
+app.get('/books/:id', async (req, res)=> {
+    try{
+        const {id} = req.params;
+        const chosenBook = await Book.findById(id);
+        return res.status(200).json(chosenBook)
+    }
+    catch(error) {
+        console.log(error.message);
+        res.status(500).send({message:error.message});
+    }
+})
+// update Book route
+app.put('/books/:id', async (req,res)=> {
+    try {
+        if(
+            !req.body.title,
+            !req.body.author,
+            !req.body.publishYear
+        ) {
+            return res.status(400).send({
+                message: 'Send all required fields'
+            }); 
+        }
+
+        const {id} = req.params;
+        const result = await Book.findByIdAndUpdate(id, req.body);
+        if(!result) {
+            return res.status(404).json({message: 'Book not found!'})
+        }
+        return res.status(200).send({message: 'Book updated sussessfully'});
+    }
+    catch(error) {
+        console.log(error.message)
+        res.status(500).send({message: error.message})
+    }
+})
 // create a new book
 app.post('/books', async (req, res)=> {
     try {
